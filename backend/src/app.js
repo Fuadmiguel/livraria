be998228b7b825c32efb4b9b99b377f5def17db2
Fuad Miguel <fuadmiguel1@gmail.com>
2025-06-09 19:56:55 -0300

    atualizando o projeto

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,12 +1,32 @@
+import path from 'path';
+import { fileURLToPath } from 'url';
+import cors from 'cors';
+
+// backend/src/app.js
 const express = require('express');
+const cors = require('cors');
+const livrosRouter = require('./routes/livros');
+const avaliacoesRouter = require('./routes/avaliacoes');
 
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
 const app = express();
-const PORT = process.env.PORT || 3000;
 
+// Middleware
+app.use(cors());
+app.use(express.json());
+
+// server estático para arquivos estáticos
+app.use(express.static(path.join(__dirname, '/home/fuad/faculdade/n46112n1/livraria-api/frontend')));
+
+// Rota raiz
 app.get('/', (req, res) => {
-  res.send('Hello, world!');
+  res.sendFile(path.join(__dirname, '/home/fuad/faculdade/n46112n1/livraria-api/frontend/index.html'));
 });
 
+app.use('/livros', livrosRouter);
+app.use('/livros', avaliacoesRouter);
+
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+    console.log(`Servidor rodando na porta ${PORT}`);
 });
