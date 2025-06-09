aa99c92a4f855858eb5e55eb7b0399edae169acf
Fuad Miguel <fuadmiguel1@gmail.com>
2025-06-09 19:35:30 -0300

    atualizando o projeto

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,13 +1,28 @@
+import express from 'express';
+import path from 'path';
+import { fileURLToPath } from 'url';
+import cors from 'cors';
+
 // backend/src/app.js
 const express = require('express');
 const cors = require('cors');
 const livrosRouter = require('./routes/livros');
 const avaliacoesRouter = require('./routes/avaliacoes');
 
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
 const app = express();
 
+// Middleware
 app.use(cors());
 app.use(express.json());
+
+// server estático para arquivos estáticos
+app.use(express.static(path.join(__dirname, '/home/fuad/faculdade/n46112n1/livraria-api/frontend')));
+
+// Rota raiz
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, '/home/fuad/faculdade/n46112n1/livraria-api/frontend/index.html'));
+});
 
 app.use('/livros', livrosRouter);
 app.use('/livros', avaliacoesRouter);
