ca2197dc08601d470d3f1b8ec58a234e9e948681
Fuad Miguel <fuadmiguel1@gmail.com>
2025-06-10 22:45:49 -0300

    Fix schema location

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,15 +1,25 @@
 import express from 'express';
 import cors from 'cors';
-import { PrismaClient } from '@prisma/client';
-import livrosRouter from './routes/livros.js'; // Verifique este caminho
+import livrosRouter from '/routes/livros.js'; // Verifique este caminho
+
+import { PrismaClient } from '@prisma/client'
 
 const prisma = new PrismaClient({
   datasources: {
     db: {
-      url: process.env.DATABASE_URL + (process.env.NODE_ENV === 'production' ? '?sslmode=require' : '')
+      url: process.env.DATABASE_URL + 
+           (process.env.NODE_ENV === 'production' ? 
+           '&sslmode=require&sslaccept=strict&pool_timeout=60' : 
+           '')
     }
-  }
+  },
+  log: ['query', 'info', 'warn', 'error']
 })
+
+// Teste de conexão
+prisma.$connect()
+  .then(() => console.log('✅ Conexão SSL estabelecida com sucesso'))
+  .catch(err => console.error('❌ Erro de conexão SSL:', err))
 
 const app = express();
 
